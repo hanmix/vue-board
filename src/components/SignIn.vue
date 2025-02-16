2b6bef6bccfad383bc16d1446178df322702f77f
Devki <hanmix2003@naver.com>
2025-02-17 00:34:30 +0900

    feat: signIn, signUp 레이아웃 및 css 추가

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
+  <div class="container">
     <h1>로그인</h1>
-    <button @click="handleSignIn">로그인</button>
-    <p>아직 아이디가 없으신가요?</p>
-    <router-link to="/signUp">회원가입</router-link>
+    <input
+      id="email"
+      class="inputbox"
+      type="text"
+      placeholder="아이디를 입력해주세요."
+      :ref="email"
+      required
+    />
+    <input
+      id="password"
+      class="inputbox"
+      type="password"
+      placeholder="비밀번호 입력해주세요."
+      :ref="password"
+      required
+    />
+    <button id="submit" @click="handleSignIn">로그인</button>
+    <div>
+      <p>아직 아이디가 없으신가요?</p>
+      <router-link to="/signUp">회원가입</router-link>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import { router } from "../routers/router";
+import { router } from "@routers/router";
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
 
 const handleSignIn = () => {
   router.push("/board");
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
+  <div class="container">
     <h1>회원가입</h1>
-    <button @click="handleSignUp">회원가입 하기</button>
+    <input
+      id="name"
+      class="inputbox"
+      type="text"
+      placeholder="이름을 입력해주세요."
+      required
+    />
+    <input
+      id="email"
+      class="inputbox"
+      type="text"
+      placeholder="아이디를 입력해주세요."
+      required
+    />
+    <input
+      id="password"
+      class="inputbox"
+      type="password"
+      placeholder="비밀번호 입력해주세요."
+      required
+    />
+    <button id="submit" @click="handleSignUp">회원가입 하기</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { router } from "../routers/router";
+import { router } from "@routers/router";
 
 const handleSignUp = () => {
   router.push("signIn");
