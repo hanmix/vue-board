fdcfb416a87ed80a2c847b9e0e6885c25dfd7171
Devki <hanmix2003@naver.com>
2025-02-17 02:07:45 +0900

    fix: 환경변수명 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_HOST = import.meta.env.VUE_APP_API_HOST;
+const API_HOST = import.meta.env.VITE_API_HOST;
 
 export const login = async (email: string, password: string) => {
   const response = await axios.post(`${API_HOST}/api/auth/login`, {
