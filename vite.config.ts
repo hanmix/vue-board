8c08b7019baa3cf546636ffc8888c0ec4fba2631
Devki <hanmix2003@naver.com>
2025-02-18 01:01:23 +0900

    refactor: 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 // import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
@@ -7,16 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@/": "/src/",
-      // "@/": fileURLToPath(new URL("./src", import.meta.url)),
-      // "@apis": fileURLToPath(new URL("./src/apis", import.meta.url)),
-      // "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-      // "@components": fileURLToPath(
-      //   new URL("./src/components", import.meta.url)
-      // ),
-      // "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
-      // "@routers": fileURLToPath(new URL("./src/routers", import.meta.url)),
-      // "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
+      '@/': '/src',
     },
   },
 });
