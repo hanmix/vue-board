1e23d7dc398887b9d334836b1adfaf0b6b14978e
한승민 <devki@learnershi.com>
2025-02-17 17:00:51 +0900

    feat: 네비게이션 가드 추가

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,33 +1,53 @@
-import { createRouter, createWebHistory } from "vue-router";
-import MainPage from "../pages/MainPage.vue";
-import BoardPage from "../pages/BoardPage.vue";
-import SignIn from "../components/SignIn.vue";
-import SignUp from "../components/SignUp.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import MainPage from '@/pages/MainPage.vue';
+import BoardPage from '@/pages/BoardPage.vue';
+import SignIn from '@/components/SignIn.vue';
+import SignUp from '@/components/SignUp.vue';
+import { useUserStore } from '@/stores/user';
+import BoardDetailPage from '@/pages/BoardDetailPage.vue';
 
 export const routes = [
   {
-    path: "/",
-    name: "main",
+    path: '/',
+    name: 'main',
     component: MainPage,
   },
   {
-    path: "/signIn",
-    name: "signIn",
+    path: '/signIn',
+    name: 'signIn',
     component: SignIn,
   },
   {
-    path: "/signUp",
-    name: "signUp",
+    path: '/signUp',
+    name: 'signUp',
     component: SignUp,
   },
   {
-    path: "/board",
-    name: "board",
+    path: '/board',
+    name: 'board',
+    meta: { requiresAuth: true },
     component: BoardPage,
+    children: [
+      {
+        path: 'detail',
+        name: 'board-detail',
+        component: BoardDetailPage,
+      },
+    ],
   },
 ];
 
-export const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const { isLoggedIn } = useUserStore();
+  const isRequiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (isRequiresAuth && !isLoggedIn) return next({ name: 'signIn' });
+  next();
+});
+
+export default router;
