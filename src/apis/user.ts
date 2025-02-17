025b195a52f0bd441e515b2d42547dfc52189d90
한승민 <devki@learnershi.com>
2025-02-17 18:39:37 +0900

    fix: API_HOST 파일명 수정

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_HOST = import.meta.env.VUE_APP_API_HOST;
+const API_HOST = import.meta.env.VITE_API_HOST;
 
 export const getUsers = async () => {
   const response = await axios.get(`${API_HOST}/api/users`);
