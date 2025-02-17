50913da76b8fd0daa57a3cb28b6b63b6daa0dfdc
한승민 <devki@learnershi.com>
2025-02-17 18:40:57 +0900

    feat: BoardDetailPage 추가

--- src/pages/BoardPage.vue
+++ src/pages/BoardPage.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <p>Board Page 입니다.</p>
+    <router-view />
+
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolores
+      tenetur libero rerum perferendis delectus velit qui maxime provident illo.
+      Quibusdam voluptas magni nisi eos cum ex magnam, voluptatem error!
+    </p>
   </div>
 </template>
 <script setup lang="ts"></script>
