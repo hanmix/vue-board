80642b511c8e166cae741cf3af6e3ce73de8559e
한승민 <devki@learnershi.com>
2025-02-17 16:59:56 +0900

    fix: import paths 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "url";
+// import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@apis": fileURLToPath(new URL("./src/apis", import.meta.url)),
-      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-      "@components": fileURLToPath(
-        new URL("./src/components", import.meta.url)
-      ),
-      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
-      "@routers": fileURLToPath(new URL("./src/routers", import.meta.url)),
-      "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
+      "@/": "/src/",
+      // "@/": fileURLToPath(new URL("./src", import.meta.url)),
+      // "@apis": fileURLToPath(new URL("./src/apis", import.meta.url)),
+      // "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      // "@components": fileURLToPath(
+      //   new URL("./src/components", import.meta.url)
+      // ),
+      // "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      // "@routers": fileURLToPath(new URL("./src/routers", import.meta.url)),
+      // "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
     },
   },
 });
