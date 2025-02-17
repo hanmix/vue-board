13567f098550f7416ef22fdc5e24bacd801b1960
한승민 <devki@learnershi.com>
2025-02-17 18:37:16 +0900

    feat: api 호출에 사용할 type 정의

---
+++ src/types/api.ts
@@ -0,0 +1,21 @@
+export interface Response {
+  isSuccess: boolean;
+  data: { token: string };
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData extends LoginData {
+  name: string;
+}
+
+export interface DecodedToken {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
