1b870ea9d45a7b62e10d6098b1816eb94e0c91ee
Devki <hanmix2003@naver.com>
2025-02-19 02:53:06 +0900

    refactor: inputValue의 빈값 체크하는 로직 리팩토링

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,13 +1,28 @@
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export const useAuth = () => {
-  const name = ref('');
-  const email = ref('');
-  const password = ref('');
+  const name = ref<string>('');
+  const email = ref<string>('');
+  const password = ref<string>('');
+  const doubleCheckPassword = ref<string>('');
+
+  const isCheckEmptyName = computed<boolean>(() => name.value.trim() === '');
+  const isCheckEmptyEmail = computed<boolean>(() => email.value.trim() === '');
+  const isCheckEmptyPassword = computed<boolean>(
+    () => password.value.trim() === ''
+  );
+  const isPasswordMatch = computed<boolean>(
+    () => password.value !== doubleCheckPassword.value
+  );
 
   return {
     name,
     email,
     password,
+    doubleCheckPassword,
+    isCheckEmptyName,
+    isCheckEmptyEmail,
+    isCheckEmptyPassword,
+    isPasswordMatch,
   };
 };
