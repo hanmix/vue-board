be23f017360e2f7098d0a79b295d85ff7a048639
한승민 <devki@learnershi.com>
2025-06-07 15:34:59 +0900

    fix: 타입스크립트가 vue 파일 인식 못 하는 현상 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 // import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
@@ -7,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@/': '/src/',
+      '@': path.resolve(__dirname, './src'),
     },
   },
   css: {
