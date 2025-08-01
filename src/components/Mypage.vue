c711b95b67120f139d6c6719c541184ac4406caf
한승민 <devki@learnershi.com>
2025-08-01 18:17:56 +0900

    fix: 마이페이지 추가

---
+++ src/components/Mypage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="board-container">
+    <div class="header">
+      <div>
+        <h1>마이페이지</h1>
+        <select v-model="selectedOption">
+          <option
+            v-for="option in options"
+            :key="option.value"
+            :value="option.value"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+      <div class="logout">
+        <button @click="logout">로그아웃</button>
+      </div>
+    </div>
+
+    <section class="posts-section">
+      <div v-if="loading" class="loading">로딩중...</div>
+
+      <div v-else-if="error" class="error">{{ error }}</div>
+
+      <div v-else-if="!postList.length" class="empty">게시글이 없습니다.</div>
+
+      <div v-else v-for="post in filteredPosts" :key="post.id">
+        <PostItem v-if="post.type === selectedOption" :post="post" />
+      </div>
+    </section>
+
+    <section class="pagination-section">
+      <Pagination v-if="totalPosts" :currentPage="page" :totalPage="lastPage" />
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+import { usePost, useUser } from '@/composables';
+
+import { computed, ref, watch } from 'vue';
+import PostItem from './PostItem.vue';
+import Pagination from './Pagination.vue';
+
+const { totalPosts, page, lastPage, postList, loading, error, fetchMyPosts } =
+  usePost();
+const { logout } = useUser();
+
+const filteredPosts = computed(() =>
+  postList.value.filter(post => post.type === selectedOption.value)
+);
+
+type selectOptions = { label: string; value: string };
+const selectedOption = ref('post');
+const options: selectOptions[] = [
+  { label: '내가 작성한 게시글', value: 'post' },
+  { label: '내가 작성한 댓글', value: 'reply' },
+];
+
+watch(
+  page,
+  () => {
+    fetchMyPosts();
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
