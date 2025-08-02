2be6a5db8efdf8d94d084b6cf5d8026f775ffd67
한승민 <devki@learnershi.com>
2025-08-02 21:10:39 +0900

    fix: main page 컴포넌트 정렬 수정

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -3,7 +3,6 @@
 <template>
   <div class="main">
     <p>Main Page 입니다.</p>
-
     <router-link to="/signIn">로그인하러 가기</router-link>
   </div>
 </template>
@@ -11,8 +10,9 @@
 <style scoped>
 .main {
   display: flex;
-  justify-content: center; /* 수평 가운데 */
-  align-items: center; /* 수직 가운데 */
+  flex-direction: column; /* 👉 요소를 세로로 나열 */
+  justify-content: center; /* 👉 세로축에서 가운데 정렬 */
+  align-items: center; /* 👉 가로축에서 가운데 정렬 */
   height: 100vh;
 }
 </style>
