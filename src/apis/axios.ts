1cfb79c87c69fda5300630549572503b53d4b39a
한승민 <devki@learnershi.com>
2025-08-04 16:36:34 +0900

    fix: axiosInstance interceptors 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,6 @@
 import axios, { type AxiosInstance } from 'axios';
-import { useRouter } from 'vue-router';
 
 const API_HOST = import.meta.env.VITE_API_HOST;
-const router = useRouter();
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_HOST,
 });
@@ -21,13 +19,30 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   response => response,
   error => {
-    if (error.response) {
-      const { status } = error.response;
-      if (status === 401) {
-        router.push('/signIn');
-      }
-      // 필요에 따라 다른 상태 코드 처리
+    const status = error.response?.status;
+
+    switch (status) {
+      case 400:
+        console.warn('잘못된 요청입니다.');
+        break;
+      case 401:
+        console.warn('로그인이 필요합니다.');
+        break;
+      case 403:
+        console.warn('접근 권한이 없습니다.');
+        break;
+      case 404:
+        console.warn('요청한 리소스를 찾을 수 없습니다.');
+        break;
+      case 500:
+        console.error('서버 에러 발생');
+        break;
+      default:
+        console.error('알 수 없는 에러:', error.message);
     }
+
+    error.__handledStatus = status;
+
     return Promise.reject(error);
   }
 );
