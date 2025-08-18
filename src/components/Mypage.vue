4c9aec49acd68a9408c89ab97f63f1ef56f24ab2
한승민 <devki@learnershi.com>
2025-08-18 13:25:41 +0900

    fix: selected options 제거

--- src/components/Mypage.vue
+++ src/components/Mypage.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="header">
     <h1>마이페이지</h1>
-    <div>
+    <div v-if="currentUser">
       내 정보
-      <li>id: {{ currentUser?.id }}</li>
-      <li>email: {{ currentUser?.email }}</li>
-      <li>name: {{ currentUser?.name }}</li>
+      <li>id: {{ currentUser.id }}</li>
+      <li>email: {{ currentUser.email }}</li>
+      <li>name: {{ currentUser.name }}</li>
     </div>
-    <select v-model="selectedOption" class="form-element">
-      <option
-        v-for="option in options"
-        :key="option.value"
-        :value="option.value"
-      >
-        {{ option.label }}
-      </option>
-    </select>
   </div>
 
   <section class="posts-section">
@@ -26,7 +17,7 @@
     <div v-else-if="!postList.length" class="empty">게시글이 없습니다.</div>
 
     <div v-else v-for="post in filteredPosts" :key="post.id">
-      <PostItem v-if="post.type === selectedOption" :post="post" />
+      <PostItem :post="post" :isMypage="isMypage" />
     </div>
   </section>
 
@@ -44,20 +35,21 @@ import { computed, onMounted, ref, watch } from 'vue';
 import PostItem from './PostItem.vue';
 import Pagination from './Pagination.vue';
 
-const { totalPosts, page, lastPage, postList, loading, error, fetchMyPosts } =
-  usePost();
+const {
+  totalPosts,
+  page,
+  lastPage,
+  postList,
+  loading,
+  error,
+  isMypage,
+  fetchMyPosts,
+} = usePost();
 const { currentUser, getUserById } = useUser();
 
 const filteredPosts = computed(() =>
-  postList.value.filter(post => post.type === selectedOption.value)
+  postList.value.filter(post => !post.isDeleted)
 );
-
-type selectOptions = { label: string; value: string };
-const selectedOption = ref('post');
-const options: selectOptions[] = [
-  { label: '내가 작성한 게시글', value: 'post' },
-  { label: '내가 작성한 답글', value: 'reply' },
-];
 
 watch(
   page,
