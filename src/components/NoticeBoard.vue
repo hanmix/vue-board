b0e57b958c68da76a42e8b0a53098f9990f54e90
한승민 <devki@learnershi.com>
2025-08-19 16:09:54 +0900

    fix: 공지게시판 추가

--- src/components/NoticeBoard.vue
+++ src/components/NoticeBoard.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>공지게시판 입니다.</div>
+  <header class="header">
+    <h1>공지게시판</h1>
+    <SearchFilter />
+  </header>
+  <section class="posts-section">
+    <div v-if="loading" class="loading">로딩중...</div>
+
+    <div v-else-if="error" class="error">{{ error }}</div>
+
+    <div v-else-if="!postList.length" class="empty">게시글이 없습니다.</div>
+
+    <div v-else v-for="post in postList" :key="post.id">
+      <PostItem :post="post" />
+    </div>
+  </section>
+
+  <section class="pagination-section">
+    <Pagination
+      v-if="totalPosts && !loading && !error"
+      :currentPage="page"
+      :totalPage="lastPage"
+    />
+  </section>
 </template>
+
+<script setup lang="ts">
+import SearchFilter from './SearchFilter.vue';
+import Pagination from './Pagination.vue';
+import PostItem from './PostItem.vue';
+import { usePost } from '@/composables';
+import { watch } from 'vue';
+
+const { loading, error, postList, page, lastPage, totalPosts, fetchPosts } =
+  usePost();
+
+watch(
+  page,
+  () => {
+    fetchPosts();
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
